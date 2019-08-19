c34bd392821425e84a49fb52575f94652cac2e0c
ahanafy <alan.hanafy@gmail.com>
2019-08-19 15:31:15 -0400

    Fix sed script and add echo with newline
    Removed apostrophes from sed line, and added a newline when appending new team directory to kustomize.yaml file

--- scripts/create-team.sh
+++ scripts/create-team.sh
@@ -15,9 +15,9 @@ cp -r "${REPO_ROOT}/cluster/${TEMPLATE}/." ${TEAM_DIR}
 
 for f in "${TEAM_DIR}*.yaml"
 do
- sed -i '' "s/$TEMPLATE/$TEAM_NAME/g" ${f}
+ sed -i "s/$TEMPLATE/$TEAM_NAME/g" ${f}
 done
 
 echo "${TEAM_NAME} created at ${TEAM_DIR}"
-echo "  - ./${TEAM_NAME}/" >> "${REPO_ROOT}/cluster/kustomization.yaml"
+echo -e "\n  - ./${TEAM_NAME}/" >> "${REPO_ROOT}/cluster/kustomization.yaml"
 echo "${TEAM_NAME} added to ${REPO_ROOT}/cluster/kustomization.yaml"
